e83d2ef24e05355cda456cb117cbb4ea2f5e03c9
juch <juch@live.cn>
2020-08-07 10:29:51 +0800

    添加参数rows,使输出行数可选

--- README.md
+++ README.md
@@ -3,7 +3,14 @@
     把整个文件夹放到网站根目录即可
 
 - 调用方法
-```$html
-<script type="text/javascript" src="//domain/hitokoto/?format=js&charset=utf-8"></script>
+``` html
+<script type="text/javascript" src="//domain/hitokoto/?format=js&charset=utf-8&rows=1"></script>
 <div id="hitokoto"><script>hitokoto()</script></div>
 ```
+- 参数
+
+|参数名|含义|
+|---|---|
+|format|输出类型,为空则仅输出文本|
+|charset|输出的编码格式|
+|rows|文本的行数,默认为单行|
--- index.php
+++ index.php
@@ -3,23 +3,33 @@ $path = dirname(__FILE__);
 $file = file($path."/全唐诗/杜甫.txt");
 
 //随机读取一行
-$arr = mt_rand( 0, count( $file ) - 1 );
-$content = str_replace("，","，<br>",trim($file[$arr]));
+$arr  = mt_rand( 0, count( $file ) - 1 );
+$content_1  = trim($file[$arr]);
+$content_2 = str_replace("，","，<br>",$content_1);
 
 //编码判断，用于输出相应的响应头部编码
 if (isset($_GET['charset']) && !empty($_GET['charset'])) {
     $charset = $_GET['charset'];
     if (strcasecmp($charset,"gbk") == 0 ) {
-        $content = mb_convert_encoding($content,'gbk', 'utf-8');
+		$content_1 = mb_convert_encoding($content_1,'gbk', 'utf-8');
+		$content_2 = mb_convert_encoding($content_2,'gbk', 'utf-8');
     }
 } else {
     $charset = 'utf-8';
 }
 header("Content-Type: text/html; charset=$charset");
 
-//格式化判断，输出js或纯文本
+//格式化判断，输出js或纯文本，如无特殊说明【rows】则发送单行文本
 if ($_GET['format'] === 'js') {
-    echo "function hitokoto(){document.write('" . $content ."');}";
+	if($_GET['rows'] == '2'){
+		echo "function hitokoto(){document.write('" . $content_2 . "');}";
+	} else {
+		echo "function hitokoto(){document.write('" . $content_1 . "');}";
+	}
 } else {
-    echo $content;
+	if($_GET['rows'] == '2'){
+		echo $content_2;
+	} else {
+    		echo $content_1;
+	}
 }
